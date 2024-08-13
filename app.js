d79b60d37341ab4206c941da6f6447ca01137da1
Arshia-Esfh <167959321+Arshia-Esfh@users.noreply.github.com>
2024-08-13 13:09:46 +0330

    add new feature

--- app.js
+++ app.js
@@ -1,34 +1,40 @@
-let userNmae=prompt("Enter your name");
-const pElem=document.querySelector('h1');
+let userNmae = prompt("Enter your name");
+const pElem = document.querySelector('h1');
 
-const arrList=[20,40,50,100];
+const arrList = [20, 40, 50, 100];
 
 
-let numberArray={
- id:1,
-  firstName:'Arshia',
-  lastName:'Nader',
-  age:20
-  
+let numberArray = {
+    id: 1,
+    firstName: 'Arshia',
+    lastName: 'Nader',
+    age: 20
+
 }
 
-function animationGenerator(){
-     console.log('Hello I am animation maker')
+function animationGenerator() {
+    console.log('Hello I am animation maker')
 }
 
-setInterval(()=>{
- console.log('Hello I am animation maker')
-},4000)
+setInterval(() => {
+    console.log('Hello I am animation maker')
+}, 4000)
 
-let post=()=>{
-  return true 
+let post = () => {
+    return true
 }
 
-let result=Math.floor()*365
+let result = Math.floor() * 365
 
 
-const color=['red','yello','green']
+const color = ['red', 'yello', 'green']
 
-function high(){
-    return true 
+function high() {
+    return true
+}
+
+
+const likePost = (userID, userPost) => {
+
+    return { post: userPost, user: userID, isLike: true }
 }
